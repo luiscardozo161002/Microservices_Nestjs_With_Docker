18999cf4a89472756d218ba41f087ceaae4c3b15
luiscardozo161002 <luiscardozo161002@gmail.com>
2024-10-31 23:16:49 -0600

    Uploading project nestjs and docker to github

---
+++ insert-examples.sql
@@ -0,0 +1,18 @@
+INSERT INTO users (username, email, password) VALUES
+('luis', 'luis_cardozo@gmail.com', 'luis2Car'),
+('gerardo', 'gerardo_cardozo@gmail.com', 'gerardo2Car');
+
+
+INSERT INTO products (name, description, price, stock) VALUES
+('Martillo', 'Martillo de Acero del 12', 49.99, 100),
+('Clavos', '1kg clavos 2 pulgadas', 39.99, 50);
+
+
+<<<<<<< HEAD
+INSERT INTO orders ("userId", "productId", "quantity", "totalPrice") VALUES
+(1, 1, 2, 49.98),  
+=======
+INSERT INTO orders ("userId", "productId", quantity, "totalPrice") VALUES
+(1, 1, 2, 49.98),
+>>>>>>> a098774 (Cambios del proyecto)
+(2, 2, 1, 39.99);
